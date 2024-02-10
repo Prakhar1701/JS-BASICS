5e6cb53bf457afde6151bd9ebcebb5a1d81ada26
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2024-02-11 04:41:50 +0530

    Primitive Types

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello World");
 
-let name = "Prakhar"; // Variable
+let name = "Prakhar"; // Variable // String Literal
 
 console.log(name);
 
@@ -9,3 +9,11 @@ const interestRate = 0.3; // Constant
 // interestRate = 1;
 
 console.log(interestRate);
+
+let age = 30; // Number Literal
+
+let isApproved = false; // Boolean Literal
+
+let firstName = undefined;
+
+let selectedName = null;
